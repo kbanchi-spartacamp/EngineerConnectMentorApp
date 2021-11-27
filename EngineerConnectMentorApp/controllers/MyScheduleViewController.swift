a8cfe8e4a085f32a3385e64447aba55c77b9f3bb
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-27 21:56:34 +0900

    fix

--- EngineerConnectMentorApp/controllers/MyScheduleViewController.swift
+++ EngineerConnectMentorApp/controllers/MyScheduleViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
+import AuthenticationServices
+import Alamofire
+import SwiftyJSON
+import KeychainAccess
+import PKHUD
 
 class MyScheduleViewController: UIViewController {
 
+    let consts = Constants.shared
+    var alert = Alert()
+    
+    var todayTimes: [Date] = []
+    
+    @IBOutlet weak var todayPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        todayPickerView.dataSource = self
+        todayPickerView.delegate = self
+        
+        setTodayTimes()
+        
+    }
+    
+    func setTodayTimes() {
+        let now = Date()
+        print(now)
+        var startTime = Calendar.current.date(byAdding: .minute, value: 30 - Calendar.current.component(.minute, from: now) % 30, to: now)
+        print(startTime)
     }
     
 
@@ -26,4 +51,26 @@ class MyScheduleViewController: UIViewController {
     }
     */
 
+}
+
+extension MyScheduleViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        todayTimes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "HH:mm", options: 0, locale: Locale(identifier: "ja_JP"))
+        return formatter.string(from: todayTimes[row])
+    }
+    
+}
+
+extension MyScheduleViewController: UIPickerViewDelegate {
+    
 }
